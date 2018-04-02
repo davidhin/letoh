0d1503a1666530365506b74ef31c1c35fc63ce7c
davidhin <hello@davidhin.com>
2018-04-02 19:40:30 +0930

    Implemented basic hotelManagement sidebar

--- javascripts/hotelmanage.js
+++ javascripts/hotelmanage.js
@@ -1,8 +1,40 @@
 // =========== MAIN FUNCTIONS ============== //
 
 $( document ).ready(function() {
+	loadHotels();
 
+	// ------------- ACCORDIAN ------------ //
+	var acc = document.getElementsByClassName("accordion");
+    var i;
+    
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+ 
 });
+
+function loadHotels() {
+	// ---------- GET OWNER'S HOTELS ---------- //
+	// e.g. for all of the owners hotels, generate the following links:
+	for (var i = 0; i < 3; i++) {
+		$('<button/>')
+	    	.text('Dynamically Added Hotel ' + i)
+			.addClass("mdl-button mdl-js-button mdl-js-ripple-effect accordion")
+			.appendTo("#myHotels");
+		var currId = "myHotel_" + i;
+		$('<div/>').attr("id", currId).addClass("panel").appendTo("#myHotels");
+		$('<a/>').click(function() { alert('foo'); }).html("General Info").addClass("mdl-navigation__link").appendTo("#" + currId);
+		$('<a/>').click(function() { alert('bar'); }).html("Room Types").addClass("mdl-navigation__link").appendTo("#" + currId);
+	}
+}
 
 // ============ OTHER =============== //
 
@@ -13,3 +45,5 @@ onload = function() {
 window.onresize = function(event) {
     sizes();
 };
+
+
