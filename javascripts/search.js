8aae039c0bfb8788362f43cf8609a7295ef376c4
davidhin <hello@davidhin.com>
2018-04-03 09:15:50 +0930

    Made the hotel cards look sweet

--- javascripts/search.js
+++ javascripts/search.js
@@ -12,21 +12,30 @@ $( document ).ready(function() {
 		$( "#minStars" ).val($("#stars").val());
 	});
 
+	hotelCards();
 });
+
+function hotelCards() {
+	for (var i = 0; i < 20; i++) {
+		var div_main = $('<div/>').addClass("demo-card-wide mdl-card mdl-shadow--2dp").appendTo("#content");
+		  // Change the background picture here
+		  var div_title = $('<div/>').addClass("mdl-card__title").appendTo(div_main).css("background", "url('https://www.w3schools.com/html/pulpitrock.jpg') center / cover");
+		  // Change the hotel name here
+			var h2_title = $('<h2/>').addClass("mdl-card__title-text").html("Hotel " + i).appendTo(div_title);
+		  // Change the hotel details here
+		  var div_content = $('<div/>').addClass("mdl-card__supporting-text").html("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifend lacinia... ").appendTo(div_main);
+		  var div_buttons = $('<div/>').addClass("mdl-card__actions mdl-card--border").appendTo(div_main);
+		  	var a_links = $('<a/>').addClass("mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect").html("More details").appendTo(div_buttons);
+		  // Change the share/favourite button here
+		  var div_menu = $('<div/>').addClass("mdl-card__menu").appendTo(div_main);
+		    var button_share = $('<button/>').addClass("mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect").appendTo(div_menu);
+			  var shareButton = $('<i/>').addClass("material-icons").html("share").appendTo(button_share);
+	}
+}
 
 // ============ OTHER =============== //
 
 onload = function() {	
-	// Add hotel cards
-	for (var i = 0; i < 9; i++) {
-		var newHotelCard = document.createElement("div");
-		var image = document.createElement("img");
-		image.src = "https://www.w3schools.com/html/pulpitrock.jpg";
-        newHotelCard.classList.add("hotelCard");
-		newHotelCard.appendChild(image);
-        document.getElementById("content").appendChild(newHotelCard);
-    }
-
     document.getElementById('check-in').value = new Date().today(false);
 	document.getElementById('check-out').value = new Date().today(true);
 	sizes();
