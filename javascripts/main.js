c180d546927914d448b66bd2fb16ccc1a6fe7606
davidhin <hello@davidhin.com>
2018-04-02 16:28:39 +0930

    Fixed up sidebar glitches, cleaned out main/style

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,30 +8,9 @@ $( document ).ready(function() {
 function sizes() {
     var headerHeight = document.getElementsByTagName("header")[0].offsetHeight;
     var footer = document.getElementsByTagName("footer")[0];
-    var sidebar = document.getElementById("sidebar");
-	var sidebarOffset = document.getElementById("sidebar").offsetWidth;
-
-    sidebar.style.marginTop = headerHeight + "px";
-    sidebar.style.height = parseInt(window.innerHeight,10) - 64 + "px";
+	var content = document.getElementById("content");
 
     // FOOTER POSITIONING 
     if (content.offsetHeight < parseInt(window.innerHeight,10) - 90) { footer.style.position = "absolute"; } 
 	else { footer.style.position = "relative"; }
-}
-
-// Menu button
-$("#sidebarButton").one("click", handler2);
-function handler1() {
-	$("#sidebar").stop().animate({ width: 0 }, 'fast', function() { 
-		$("#sidebar").css("padding", "0px");
-	    sizes();
-	});
-	$(this).one("click", handler2);
-}
-function handler2() {
-	$("#sidebar").css("padding", "20px");
-	$("#sidebar").stop().animate({ width: "100%" }, 'fast', function() { 
-		sizes();
-	}); 
-    $(this).one("click", handler1);
 }
