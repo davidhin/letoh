b6c806b1ccde310effbd4ca68171365fc743f60c
CornDogBodyMan <stupidjl.lee@gmail.com>
2018-04-08 11:32:55 +0930

    Changing hotel js javascript to jQuery

--- javascripts/hotel.js
+++ javascripts/hotel.js
@@ -209,44 +209,26 @@ function get_bookings(can_change) {
 	  .addClass("mdl-cell mdl-card mdl-shadow--2dp mdl-cell--4-col-desktop mdl-cell--8-col-tablet mdl-cell--4-col-phone")
 	  .append($('<h3 class="hotelboxheadings">Your review</h3>'))
 	  .append($('<button class="reviewButton" onclick="reviewButton(this)">+</button>'))
-	  .append($('<button class="postButton" onclick="postButton(this)">Post</button>'))
-	  .append($('<button class="cancelButton" onclick="cancelButton(this)">Cancel</button>'))
 	  .appendTo(book_container);
 }
 
 function editBookingedit(index){
-  var date = document.createElement("INPUT");
-  var form = document.createElement("FORM");
-  date.setAttribute("class","dateInput");
-  date.setAttribute("type","date");
-  date.style.display="inline";
-  form.appendChild(date);
-
-  var dates = document.createElement("INPUT");
-  var forms = document.createElement("FORM");
-  dates.setAttribute("class","dateInput");
-  dates.setAttribute("type","date");
-  dates.style.display="inline";
-  forms.appendChild(dates);
-  var span = index.parentElement.getElementsByClassName("tableFill");
-
-  span[0].appendChild(form);
-  span[1].appendChild(forms);
-
-  var confirm = index.parentElement.getElementsByClassName("confirmBookingedit");
-  var cancel = index.parentElement.getElementsByClassName("cancelBookingedit");
-  confirm[0].style.display = "block";
-  cancel[0].style.display = "block";
   index.style.display = "none";
+  var spanZ = index.parentElement.getElementsByClassName("tableFill")[0];
+  $("<form><input class='dateInput' type='date' style='display:inline'></input></form>").appendTo(spanZ);
+  var spanO = index.parentElement.getElementsByClassName("tableFill")[1];
+  $("<form><input class='dateInput' type='date' style='display:inline'></input></form>").appendTo(spanO);
+
+  $(index).closest('.boxtable').children('.confirmBookingedit').css('display','block');
+  $(index).closest('.boxtable').children('.cancelBookingedit').css('display','block');
 }
 
 function confirmBookingedit(index){
-  var date = index.parentElement.getElementsByClassName("dateInput");
-  var edit = index.parentElement.getElementsByClassName("editBookingedit");
-  var cancel = index.parentElement.getElementsByClassName("cancelBookingedit");
   index.style.display = "none";
-  cancel[0].style.display = "none";
-  edit[0].style.display = "block";
+  $(index).closest('.boxtable').children(".cancelBookingedit").css('display','none');
+  $(index).closest('.boxtable').children(".editBookingedit").css('display','block');
+
+  var date = index.parentElement.getElementsByClassName("dateInput");
 
   if(date[1].value){
     date[1].parentElement.parentElement.innerText = date[1].value;
@@ -262,17 +244,14 @@ function confirmBookingedit(index){
     date[1].parentElement.removeChild(date[1]);
     date[0].parentElement.removeChild(date[0]);
   }
-
 }
 
 function cancelBookingedit(index){
-  var date = index.parentElement.getElementsByClassName("dateInput");
-  var confirm = index.parentElement.getElementsByClassName("confirmBookingedit");
-  var edit = index.parentElement.getElementsByClassName("editBookingedit");
-  confirm[0].style.display = "none";
   index.style.display = "none";
-  edit[0].style.display = "block";
+  $(index).closest('.boxtable').children(".confirmBookingedit").css('display','none');
+  $(index).closest('.boxtable').children(".editBookingedit").css('display','block');
 
+  var date = index.parentElement.getElementsByClassName("dateInput");
   date[1].parentElement.removeChild(date[1]);
   date[0].parentElement.removeChild(date[0]);
 }
@@ -280,49 +259,29 @@ function cancelBookingedit(index){
 function reviewButton(index){
   index.style.display="none";
   var review = index.parentElement;
-  var postbutton=review.getElementsByClassName("postButton");
-  var cancelbutton=review.getElementsByClassName("cancelButton");
 
-  var box = document.createElement("TEXTAREA");
-  box.style.resize="none";
-  box.style.width="100%";
-  box.style.height="80%";
-  box.style.display="block";
-  review.insertBefore(box, postbutton[0]);
-
-  postbutton[0].style.display="block";
-  cancelbutton[0].style.display="inline";
+  $('<textarea style="resize:none;width:100%;height:80%;display:block;"></textarea>').appendTo(review);
+  $('<button class="postButton" onclick="postButton(this)" style="display:block">Post</button>').appendTo(review);
+  $('<button class="cancelButton" onclick="cancelButton(this)" style="display:inline">Cancel</button>').appendTo(review);
 }
 
 function postButton(index){
   var review = index.parentElement;
   var textbox = index.parentElement.getElementsByTagName("TEXTAREA");
-  var postbutton=review.getElementsByClassName("postButton");
-  var cancelbutton=review.getElementsByClassName("cancelButton");
-  var para = document.createElement("P");
-  para.className="boxparagraph";
-  para.style.height="100%";
-  para.style.paddingBottom = "0px";
-  para.style.marginBottom = "30px;";
-  para.style.overflow = "auto";
-  para.innerText = textbox[0].value;
 
-  review.insertBefore(para, postbutton[0]);
-  textbox[0].style.display="none";
-  postbutton[0].style.display="none";
-  cancelbutton[0].style.display="none";
+  var para = $('<p class="boxparagraph" style="height:100%;paddingBottom:0px;marginBottom:30px;overflow:auto;"></p>').text(textbox[0].value);
+  $(para).appendTo(review);
+
+  $(index).closest('.reviewmodule').children("textarea").css('display','none');
+  $(index).closest('.reviewmodule').children(".postButton").css('display','none');
+  $(index).closest('.reviewmodule').children(".cancelButton").css('display','none');
 }
 
 function cancelButton(index){
-  var review = index.parentElement;
-  var textbox = index.parentElement.getElementsByTagName("TEXTAREA");
-  var reviewbutton=review.getElementsByClassName("reviewButton");
-  var postbutton=review.getElementsByClassName("postButton");
-  var cancelbutton=review.getElementsByClassName("cancelButton");
-  textbox[0].parentNode.removeChild(textbox[0]);
-  postbutton[0].style.display="none";
-  cancelbutton[0].style.display="none";
-  reviewbutton[0].style.display="block";
+  $(index).closest('.reviewmodule').children('.reviewButton').css('display','block');
+  $(index).closest('.reviewmodule').children('.postButton').remove();
+  $(index).closest('.reviewmodule').children('textarea').remove();
+  $(index).remove();
 }
 
 function remove(index){
@@ -336,14 +295,12 @@ function remove(index){
 
 /* ================== Account Page ===================== */
 function accountChange(index){
-  var setting = index.parentElement.getElementsByTagName("input");
+  index.style.display="none";
+  $(index).closest('.settingDescription').children('input').css('display','block');
+
   var buttons = index.parentElement.getElementsByTagName("button");
-  for(var i=0;i<setting.length;i++){
-    setting[i].style.display="block";
-  }
   buttons[1].style.display="block";
   buttons[2].style.display="inline";
-  index.style.display="none";
 }
 
 function accountConfirm(index){
@@ -366,125 +323,17 @@ function accountConfirm(index){
     }
   }
 
-  for(var s=0;s<setting.length;s++){
-    setting[s].style.display="none";
-  }
+  $(index).closest('.settingDescription').children('input').hide();
   buttons[0].style.display="block";
   buttons[1].style.display="none";
   buttons[2].style.display="none";
 }
 
 function accountCancel(index){
-  var setting = index.parentElement.getElementsByTagName("input");
+  $(index).closest('.settingDescription').children('input').hide();
+
   var buttons = index.parentElement.getElementsByTagName("button");
-  for(var i=0;i<setting.length;i++){
-    setting[i].style.display="none";
-  }
   buttons[0].style.display="block";
   buttons[1].style.display="none";
   buttons[2].style.display="none";
 }
-
-//Making Current Bookings modules
-//  $("#currentBookings")
-//    .append($("<div></div>")
-//      .attr('class','modulecontainer')
-//      //Cancel Booking Button
-//      .append('<button class="removeBookingButton" onclick="remove(this)">X</button>')
-
-//      //Image Module
-//      .append($("<div class='divstyle imagemodule'></div>")
-//        .append($("<img alt='Hotel' title='Your Hotel' class='boximage'>")
-//          .attr('src','images/letoh1.jpg')//CONTENT
-//        )
-//      )
-//      //description module
-//      .append($("<div></div>")
-//        .attr('class','divstyle descriptmodule')
-//        .append($("<h3 class='hotelboxheadings'></h3>")
-//          .text('Paradise Interchange Hotel')
-//        )
-//        .append('<h3 class="boxheadings">Location:</h3>')
-//        .append($('<p class="boxparagraph"></p>')
-//          .text("12 Jimmy Street")//Content
-//        )
-//        .append($('<p class="boxparagraph"></p>')
-//          .text("Adelaide, South Australia")//Content
-//        )
-//        .append($('<p class="boxparagraph"></p>')
-//          .text("Room 1: 2 Adults, 1 King bed")//Content
-//        )
-//
-//        .append("<br>")
-//        .append($('<h4 class="boxheadings">Your Booking Includes:</h4>'))
-//        .append($('<ul class="boxparagraph"></ul>')
-//          .append($('<li></li>')
-//            .text("1 Bathroom")//CONTENT
-//          )
-//          .append($('<li></li>')
-//            .text("Free Continental Breakfast")//CONTENT
-//          )
-//          .append($('<li></li>')
-//            .text("Free Wifi")//CONTENT
-//          )
-//          .append($('<li></li>')
-//            .text("Free Parking")//CONTENT
-//          )
-//        )
-//        .append("<br>")
-//        //Table
-//        .append($('<table class="boxtable"></table>')
-//          .append($('<tr></tr>')
-//            .append('<td>Check-in:</td>')
-//            .append($('<td class="tablerightcol tableFill"></td>')
-//              .text("2018-05-15")//CONTENT
-//            )
-//          )
-//          .append($('<tr></tr>')
-//            .append('<td>Check-out:</td>')
-//            .append($('<td class="tablerightcol tableFill"></td>')
-//              .text("2018-05-19")//CONTENT
-//            )
-//          )
-//          .append($('<tr></tr>')
-//            .append('<td>Length of stay:</td>')
-//            .append($('<td class="tablerightcol"></td>')
-//              .text("4 days")//CONTENT
-//            )
-//          )
-//          .append($('<tr></tr>')
-//            .append($('<td></td>')
-//              .text("4 days")//CONTENT
-//            )
-//            .append($('<td class="tablerightcol"></td>')
-//              .text("AU $5000")//CONTENT
-//            )
-//          )
-//          .append($('<tr></tr>')
-//            .append('<td>Taxes</td>')
-//            .append($('<td class="tablerightcol"></td>')
-//              .text("AU $54000")//CONTENT
-//            )
-//          )
-//          .append($('<tr></tr>')
-//            .append('<th class="tabletotal">Total</th>')
-//            .append($('<th class="tablerightcol"></th>')
-//              .text("AU $59000")//CONTENT
-//            )
-//          )
-//          .append('<button class="editBookingedit" onclick="editBookingedit(this)">Change</button>')
-//          .append('<button class="confirmBookingedit" onclick="confirmBookingedit(this)">Confirm</button>')
-//          .append('<button class="cancelBookingedit" onclick="cancelBookingedit(this)">Cancel</button>')
-//        )//table end
-//      )
-//  );
-
-
-	//	//Review Module
-//	.append($("<div></div>")
-//	  .attr('class','divstyle reviewmodule')
-//	  .append($('<h3 class="hotelboxheadings">Your review</h3>'))
-//	  .append($('<button class="reviewButton" onclick="reviewButton(this)">+</button>'))
-//	  .append($('<button class="postButton" onclick="postButton(this)">Post</button>'))
-//	  .append($('<button class="cancelButton" onclick="cancelButton(this)">Cancel</button>'))
-//	)
