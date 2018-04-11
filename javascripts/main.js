6f2832cc6345cb1855405bb02a76f255eb62ecd5
davidhin <hello@davidhin.com>
2018-04-11 15:28:21 +0930

    Validated all HTML files

--- javascripts/main.js
+++ javascripts/main.js
@@ -36,7 +36,7 @@ function includeHTML() {
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    file = elmnt.getAttribute("data-w3-include-html");
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
@@ -45,7 +45,7 @@ function includeHTML() {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
+          elmnt.removeAttribute("data-w3-include-html");
           includeHTML();
         }
       }
